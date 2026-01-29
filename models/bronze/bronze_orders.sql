c21448741625d7b92d296ec7482d195cf5fe13b3
Ilya Gridin <ilya.gridin24@gmail.com>
2026-01-29 08:51:39 +0000

    added incremental materialization to bronze_orders

--- models/bronze/bronze_orders.sql
+++ models/bronze/bronze_orders.sql
@@ -1,3 +1,14 @@
-select
+{{
+  config(
+         materialized = 'incremental',
+         unique_key = 'id'
+         )
+}}
+SELECT
 *
-from {{ source('landing', 'orders') }}
+FROM 
+{{ source('landing', 'orders') }}
+
+{% if is_incremental() %}
+where created_at > (select coalesce(max(created_at),'1900-01-01') from {{ this }})
+{% endif %}
